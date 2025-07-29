e871e49a63c0d414701dbff16582bf860a059482
Maria Kesa <maria.kesa@gmail.com>
2025-07-29 23:18:52 +0300

    first steps

---
+++ notebook.jl
@@ -0,0 +1,33 @@
+### A Pluto.jl notebook ###
+# v0.20.13
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ c459d370-6cb7-11f0-1307-6d36d49731ca
+1+1
+
+# ╔═╡ dffdc3f5-d89f-4ef6-b55a-8debcd58962d
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.11.6"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═c459d370-6cb7-11f0-1307-6d36d49731ca
+# ╠═dffdc3f5-d89f-4ef6-b55a-8debcd58962d
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
